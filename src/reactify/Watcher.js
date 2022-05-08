9d0f4cc13e9184924af39221e21bdff1da88f401
RYZZ <1378564241@qq.com>
2022-05-08 19:28:35 +0800

    feat: update

--- src/reactify/Watcher.js
+++ src/reactify/Watcher.js
@@ -9,7 +9,9 @@ export class Watcher {
   constructor(data, expression, cb) {
     // data 依赖数据对象
     // expression 依赖的值
-    // 依赖变化时的行为
+    // cb 依赖变化时的行为
+
+    // 初始化
     this.data = data
     if (typeof expression === 'function') {
       this.getter = expression
@@ -18,30 +20,23 @@ export class Watcher {
     }
     this.cb = cb
 
+    // 取值
     this.value = this.get({ init: true }) // 初始化watcher时立刻订阅依赖
   }
-  get({ init }) { 
+  get({ init }) {
     // 只在初始化 watcher 的时候收集依赖
     init && pushTarget(this)
-    var value = this.getter(this.data) // 触发依赖收集
+    const value = this.getter(this.data) // 触发依赖收集
     init && popTarget(this)
     return value
   }
   update() {
-    var value = this.get({ init: false }) // 不要再触发依赖收集
-    // 如果getter是渲染函数，那么value和this.value都是undefined（渲染函数不返回值），不进入if语句
-    // 对于数组，由于是引用类型，value !== this.value肯定不成立，所以要修正它
-    if (value !== this.value || typeof value === 'object') {
-      var oldValue = this.value
-      var newValue = value
+    const value = this.get({ init: false }) // 不要再触发依赖收集
+    if (value !== this.value) {
+      const oldValue = this.value
+      const newValue = value
       this.value = newValue
       this.cb.call(this.data, oldValue, newValue)
     }
-  }
-  addDep(dep) {
-
-  }
-  cleanUpDeps() {
-
   }
 }
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -4,7 +4,8 @@ import _ from '../lib/underscore.1.13.2.esm.js'
  * 链式读取一个对象，使用underscore，它能处理很多错误值
  * 如果传入 _.get 的 keyPath 是一个空数组，返回默认值
  */
-export const chainGet = (data, keyPath, defaultValue = void 0) => _.get(data, keyPath.split('.'), defaultValue)
+export const chainGet = (data, keyPath, defaultValue = void 0) =>
+  _.get(data, keyPath.split('.'), defaultValue)
 
 /**
  * 链式赋值一个对象
@@ -13,9 +14,9 @@ export const chainSet = (data, keyPath, value) => {
   const keyPathArray = keyPath.split('.')
   const targetKey = keyPathArray.pop()
   var targetValue = null
-  if (keyPathArray.length > 0){
+  if (keyPathArray.length > 0) {
     targetValue = _.get(data, keyPathArray, null)
-  }else{
+  } else {
     targetValue = data
   }
   return targetValue ? (targetValue[targetKey] = value) : void 0
@@ -30,8 +31,44 @@ export function parsePath(path) {
   }
 }
 
-export function setReactiveValue(target, prop, value) {
-  // === Vue.set
-  target[prop] = value
-  target.__ob__.dep.notify()
+export function setValueWithReactive(target, key, value) {
+  // 对于数组利用splice实现添加元素
+  if (Array.isArray(target)) {
+    // 如果splice索引超过数组长度会报错
+    target.length = Math.max(target.length, key)
+    target.splice(key, 1, value)
+    return value
+  }
+  // 对于对象，如果该属性已经存在，直接赋值
+  if (key in target) {
+    target[key] = value
+    return value
+  }
+  const ob = target.__ob__
+  // 如果目标对象不是响应式对象，直接赋值
+  if (!ob) {
+    target[key] = value
+    return value
+  }
+  // 响应式化
+  defineReactive(target, key, value)
+  // 触发更新
+  ob.dep.notify()
+  return value
+}
+
+export function deleteValueWithReactive(target, key) {
+  // 对于数组用splice方法删除元素
+  if (Array.isArray(target)) {
+    target.splice(key, 1)
+    return
+  }
+  const ob = target.__ob__
+  // 如果对象没有该属性，直接返回
+  if (!(key in target)) return
+  delete target[key]
+  // 如果不是响应式对象，则不需要触发更新
+  if (!ob) return
+  // 对于响应式对象，删除属性后要触发更新
+  ob.dep.notify()
 }
